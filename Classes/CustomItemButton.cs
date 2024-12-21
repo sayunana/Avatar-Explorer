6afff5afd3191a974753b9f531170f3646b90590
hiwaitan <pcweb001@gmail.com>
2024-12-22 06:53:40 +0900

    不要なusingの削除

--- Classes/CustomItemButton.cs
+++ Classes/CustomItemButton.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Avatar_Explorer.Classes
+﻿namespace Avatar_Explorer.Classes
 {
     internal class CustomItemButton : Button
     {
