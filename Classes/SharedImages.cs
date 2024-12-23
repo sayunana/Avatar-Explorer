fd3cda4bcfd313b6e1e9d589441361c12dab996e
hiwaitan <pcweb001@gmail.com>
2024-12-23 14:28:07 +0900

    ボタンの画像表示のメモリリークを修正 共有リソースの画像を1つのクラスにまとめました

---
+++ Classes/SharedImages.cs
@@ -0,0 +1,49 @@
+﻿namespace Avatar_Explorer.Classes
+{
+    public class SharedImages
+    {
+        private static readonly Image FileImage = Image.FromStream(new MemoryStream(Properties.Resources.FileIcon));
+        private static readonly Image FolderImage = Image.FromStream(new MemoryStream(Properties.Resources.FolderIcon));
+        private static readonly Image CopyImage = Image.FromStream(new MemoryStream(Properties.Resources.CopyIcon));
+        private static readonly Image TrashImage = Image.FromStream(new MemoryStream(Properties.Resources.TrashIcon));
+        private static readonly Image EditImage = Image.FromStream(new MemoryStream(Properties.Resources.EditIcon));
+        private static readonly Image OpenImage = Image.FromStream(new MemoryStream(Properties.Resources.OpenIcon));
+
+        public enum Images
+        {
+            FileIcon,
+            FolderIcon,
+            CopyIcon,
+            TrashIcon,
+            EditIcon,
+            OpenIcon
+        };
+
+        public static Image GetSharedImage(Images images)
+        {
+            Image sharedImage = images switch
+            {
+                Images.FileIcon => FileImage,
+                Images.FolderIcon => FolderImage,
+                Images.CopyIcon => CopyImage,
+                Images.TrashIcon => TrashImage,
+                Images.EditIcon => EditImage,
+                Images.OpenIcon => OpenImage,
+                _ => throw new ArgumentOutOfRangeException(nameof(images), images, "共有画像の定義がありません")
+            };
+            return sharedImage;
+        }
+
+        public static bool IsSharedImage(Image image)
+        {
+            if(image == FileImage
+                || image == FolderImage 
+                || image == CopyImage 
+                || image == TrashImage 
+                || image == EditImage 
+                || image == OpenImage)
+                return true;
+            return false;
+        }
+    }
+}
